9e6996b67644b75816ffdd8fedaac771ff416980
Makarov ilya <74796903+Ilya-Kravchenko@users.noreply.github.com>
2022-03-03 09:50:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ function addition() {
 console.log(number)
 addition()
 console.log(addition())
+
+
+const number = {
+  one: 1,
+}
+
+function addition() {
+  const ten = 10
+  const newNumber = Object.assign({}, number)
+  newNumber.one += 1
+  newNumber.two -= 10
+  return newNumber
+}
+
+console.log(number)
+addition()
+console.log(addition())
