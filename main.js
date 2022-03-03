48ff54a8ab36375d029d7a40d3e53f0da82872db
Makarov ilya <74796903+Ilya-Kravchenko@users.noreply.github.com>
2022-03-03 09:17:25 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const number = {
+  one: 1,
+}
+
+function addition() {
+  const ten = 10
+  const newNumber = Object.assign({}, number)
+  newNumber.one += 1
+  newNumber.two -= 10
+  return newNumber
+}
+
+console.log(number)
+addition()
+console.log(addition())
